012f31d998c1b8702bb884166129b98c6e425b60
JB Ladera <minidomou@gmail.com>
2022-07-05 16:51:46 -0500

    Add reminder.drop to user schema

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,10 +1,19 @@
 import { model, Schema } from 'mongoose';
 
+function createMap() {
+    return new Map();
+}
+
+const reminderSchema = new Schema({
+    drop: { type: Boolean, default: false },
+});
+
 const userSchema = new Schema({
     id: { type: String, required: true },
-    seriesIds: { type: Map, of: Boolean, required: true },
-    guildIds: { type: Map, of: Boolean, required: true },
-    globalIds: { type: Map, of: String, required: true },
+    seriesIds: { type: Map, of: Boolean, required: true, default: createMap },
+    guildIds: { type: Map, of: Boolean, required: true, default: createMap },
+    globalIds: { type: Map, of: String, required: true, default: createMap },
+    reminder: { type: reminderSchema, required: true, default: {} },
 }, { timestamps: true });
 
 export const User = model('User', userSchema);
