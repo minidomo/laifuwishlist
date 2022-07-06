83c9e9a5b6d696d8e8099f081cebc3ad20fcafad
JB Ladera <minidomou@gmail.com>
2022-07-05 23:37:23 -0500

    Switched to dotenv for config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,18 @@
-import * as process from 'node:process';
-import * as config from './config.json';
+import { argv, env } from 'node:process';
 
-if (!process.argv.some(arg => arg === '--prod' || arg === '--dev')) {
+if (!argv.some(arg => arg === '--prod' || arg === '--dev')) {
     throw new Error('--prod or --dev must be passed as an argument');
 }
 
-export const production: boolean = process.argv.includes('--prod');
+export const production: boolean = argv.includes('--prod');
 
-export const token: string = production ? config.prod.token : config.dev.token;
+export const token = production ? env.PROD_DISCORD_TOKEN : env.DEV_DISCORD_TOKEN;
 
-export const guildsIds: string[] = production ? config.prod.guildIds : config.dev.guildIds;
+const guildIdsString = production ? env.PROD_GUILD_IDS : env.DEV_GUILD_IDS;
+export const guildsIds = guildIdsString.split(',');
 
-export const clientId: string = production ? config.prod.clientId : config.dev.clientId;
+export const clientId = production ? env.PROD_DISCORD_CLIENT_ID : env.DEV_DISCORD_CLIENT_ID;
 
-export const ownerClientId: string = config.owner.clientId;
+export const ownerClientId = env.OWNER_CLIENT_ID;
 
-export const databaseUri: string = config.database.uri;
+export const databaseUri = production ? env.PROD_DATABASE_URI : env.DEV_DATABASE_URI;
